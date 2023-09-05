9182b0661c52f9a584f36ed749ac5af0885b2c84
coderdostarjun <khanalarjun225@gmail.com>
2023-09-05 18:04:47 +0545

    bulb project

--- script .js
+++ script .js
@@ -1,20 +1,16 @@
-// 4 pillar of DOM
-// 1. selection of an Element
-let check=document.querySelector("h1");
-console.log(check);
-
-// 2. changing HTML
-let change=document.querySelector(".pg");
-// console.log(change);
-change.innerHTML="changed html";
-// 3. changing CSS
-check.style.color="blue";
-check.style.backgroundColor="yellow";
-// 4.Event listner
-var butt=document.querySelector(".btn");
-butt.addEventListener("click",function()
+let bulb=document.querySelector("#bulb"); //select div container
+let btn=document.querySelector("button");//select button element
+let flag=0;
+btn.addEventListener("click",()=>
 {
- butt.innerHTML="new button";
- butt.style.color="red";
- butt.style.backgroundColor="yellow";
+    if(flag==0)
+    {
+        bulb.style.backgroundColor="yellow";
+        flag=1;
+    }
+    else
+    {
+        bulb.style.backgroundColor="black"
+        flag=0;
+    }
 })
